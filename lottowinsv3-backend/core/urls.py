72978dc6216d50ab9cccf2c3ba28aa8b73b0d4e1
gustavonew2010@gmail.com <gustavonew2010@gmail.com>
2025-05-15 23:17:04 -0300

    ajustes

--- lottowinsv3-backend/core/urls.py
+++ lottowinsv3-backend/core/urls.py
@@ -3,16 +3,6 @@ URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -32,18 +22,19 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=(permissions.AllowAny,),  # Permite acesso público ao Swagger
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/lottery/', include('lotterydata.urls')),
+    
+    # Documentação pública
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('accounts/login/', __import__('django.contrib.auth.views').contrib.auth.views.LoginView.as_view(), name='login'),
 ]
 
-# Adiciona URLs para servir arquivos de mídia em desenvolvimento
+# Serve arquivos de mídia em desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
